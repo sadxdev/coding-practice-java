db5f78114b617c3672623a9c50b372c08c057dbf
sadxdev <sadikxdev@gmail.com>
2025-08-30 21:13:22 +0530

    binary addition updated

--- java8/src/main/java/com/sadik/practice/java8/w3resource/basic/BinaryAddition.java
+++ java8/src/main/java/com/sadik/practice/java8/w3resource/basic/BinaryAddition.java
@@ -12,16 +12,40 @@ import java.util.Scanner;
 
 public class BinaryAddition {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        long binary1 = 0, binary2 = 0;
+        int i = 0, remainder = 0;
+
+        int[] sum = new int[20];
+
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Input first binary number: ");
-        int firstNumber = input.nextInt();
-        System.out.println("");
+        binary1 = in.nextLong();
 
         System.out.println("Input second binary number: ");
-        int secondNumber = input.nextInt();
-        System.out.println("");
+        binary2 = in.nextLong();
 
-        System.out.println("Sum of two binary numbers: " );
+        while(binary1 != 0 || binary2 != 0){
+            sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
+            remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) /2);
+            binary1 = binary1 / 10;
+            binary2 = binary2 / 10;
+        }
 
+        if(remainder != 0){
+            sum[i++] = remainder;
+        }
+
+        --i;
+
+        System.out.println("Sum of two binary numbers: ");
+        while(i >= 0){
+            System.out.print(sum[i--]);
+        }
+
+        System.out.print("\n");
+
+        in.close();
     }
 }
