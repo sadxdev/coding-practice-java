94d9aa852b2a07914b6704098605ea58a9b0de26
sadxdev <sadikxdev@gmail.com>
2025-09-05 23:11:40 +0530

    Defanging an IP address - #1108

---
+++ java8/src/main/java/com/sadik/practice/java8/leetcode/easy/DefangingAnIpAddress.java
@@ -0,0 +1,24 @@
+package com.sadik.practice.java8.leetcode.easy;
+
+public class DefangingAnIpAddress {
+    public static void main(String[] args) {
+        String address = "1.1.1.1";
+        System.out.println(ipdefang(address));
+    }
+
+    public static String ipdefang(String address){
+        StringBuilder sb = new StringBuilder(address.length() + 6);
+
+        for(int i = 0; i < address.length(); i++){
+            char c = address.charAt(i);
+            if(c == '.'){
+                sb.append('[').append('.').append(']');
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
