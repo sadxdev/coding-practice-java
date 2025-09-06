f45c36c6554e772f42f52927fa5dd53123759faf
sadxdev <sadikxdev@gmail.com>
2025-09-06 19:41:43 +0530

    Valid Anagram - #242

---
+++ java8/src/main/java/com/sadik/practice/java8/leetcode/easy/ValidAnagram.java
@@ -0,0 +1,26 @@
+package com.sadik.practice.java8.leetcode.easy;
+
+import java.util.Arrays;
+
+// #242
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String str1 = "alert";
+        String str2 = "laterr";
+        System.out.println(areAnagrams(str1, str2));
+    }
+
+    public static boolean areAnagrams(String str1, String str2){
+       if(str1.length() != str2.length()){
+           return false;
+       }
+
+       char[] arr1 = str1.toCharArray();
+       char[] arr2 = str2.toCharArray();
+
+       Arrays.sort(arr1);
+       Arrays.sort(arr2);
+
+       return Arrays.equals(arr1, arr2);
+    }
+}
