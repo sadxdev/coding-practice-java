d04f94ecbd5e5f6b59c4c53d33bae192c0cf994a
sadxdev <sadikxdev@gmail.com>
2025-09-12 20:41:11 +0530

    String Rotations - # 796

---
+++ java8/src/main/java/com/sadik/practice/java8/leetcode/easy/StringRotations.java
@@ -0,0 +1,24 @@
+package com.sadik.practice.java8.leetcode.easy;
+
+public class StringRotations {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+
+        System.out.println(areRotations(s, goal));
+    }
+
+    private static boolean areRotations(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+
+        String doubled = s + s;
+
+        if(doubled.contains(goal)){
+            return true;
+        } else {
+            return false;
+        }
+
+//        return s.length() == goal.length() && (s + s).contains(goal);
+    }
+}
