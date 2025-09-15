ec46a76f02c77d567095f9949fd802239f79b629
sadxdev <sadikxdev@gmail.com>
2025-09-15 17:44:23 +0530

    Move Zeros - LC 283

---
+++ java8/src/main/java/com/sadik/practice/java8/leetcode/easy/MoveZeros.java
@@ -0,0 +1,26 @@
+package com.sadik.practice.java8.leetcode.easy;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        moveZeros(nums);
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i]);
+            if(i < nums.length - 1) System.out.print(",");
+        };
+    }
+
+    private static void moveZeros(int[] nums) {
+        int write = 0;
+
+        for(int read = 0; read < nums.length; nums++){
+            if(nums[read] != 0){
+                nums[write++] = nums[read];
+            }
+        }
+
+        while(write < nums.length){
+            nums[write++] = 0;
+        }
+    }
+}
