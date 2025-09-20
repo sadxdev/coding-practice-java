30838a92f2eb97410b0b7844132bccf37e008d04
sadxdev <sadikxdev@gmail.com>
2025-09-20 22:12:12 +0530

    Maximum Average sub array I - #643

---
+++ java8/src/main/java/com/sadik/practice/java8/leetcode/easy/MaximumAverageSubArrayI.java
@@ -0,0 +1,23 @@
+package com.sadik.practice.java8.leetcode.easy;
+
+public class MaximumAverageSubArrayI {
+    public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println(findMaxAverage(nums, k));
+    }
+
+    public static double findMaxAverage(int[] nums, int k){
+        double sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+        for(int i = k; i < nums.length; i++){
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum / k ;
+    }
+}
